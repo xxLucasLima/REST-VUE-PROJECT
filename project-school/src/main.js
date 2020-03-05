100ae1ebaed85afb471678d478e5fde115fbd217
xxLucasLima <lucas.lima.pinheiro@hotmail.com>
2020-03-05 16:27:59 -0300

    teste

--- project-school/src/main.js
+++ project-school/src/main.js
@@ -1,22 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
-import VueResource from 'vue-resource';
-
-Vue.config.productionTip = false
-Vue.use(VueResource);
-
-new Vue({
-  render: h => h(App),
-=======
 import router from './router'
 import VueResource from 'vue-resource';
 
 Vue.config.productionTip = false
 Vue.use(VueResource); 
-
 new Vue({
   router,
   render: h => h(App)
->>>>>>> Alterações em aluno e professor
 }).$mount('#app')
